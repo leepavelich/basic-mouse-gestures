b21b6e0f4b10b2fbe01ee07c00c677e1d8ec21d1
Lee Pavelich <leepavelich@gmail.com>
2024-07-22 10:48:33 -0600

    Open background tabs in adjacent tab

--- background.js
+++ background.js
@@ -2,7 +2,11 @@ const gestureActions = {
   'left': (sender) => chrome.tabs.goBack(sender.tab.id),
   'right': (sender) => chrome.tabs.goForward(sender.tab.id),
   'up': (_sender, link) => chrome.tabs.create({ url: link || 'chrome://newtab/', active: true }),
-  'down': (_sender, link) => chrome.tabs.create({ url: link || 'chrome://newtab/', active: false }),
+  'down': (sender, link) => {
+    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      chrome.tabs.create({ url: link || 'chrome://newtab/', active: false, index: tabs[0].index + 1 });
+    });
+  },
   'down-right': (sender) => chrome.tabs.remove(sender.tab.id),
 };
 
