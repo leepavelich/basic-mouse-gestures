d52afa2ac06c2200a8358848fd46727716d3e56b
Lee Pavelich <leepavelich@gmail.com>
2024-07-26 16:33:17 -0600

    Error handling for forward gesture

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
+const Errors = {
+  NOT_FOUND_NEXT_PAGE: "Cannot find a next page in history.",
+};
+
+const forwardErrHandler = (error) => {
+  if (error.message === Errors.NOT_FOUND_NEXT_PAGE) return
+  console.error(`Error performing 'right' gesture: ${error.message}`);
+}
+
 const gestureActions = {
   'left': (sender) => chrome.tabs.goBack(sender.tab.id),
-  'right': (sender) => chrome.tabs.goForward(sender.tab.id),
+  'right': (sender) => chrome.tabs.goForward(sender.tab.id).catch(forwardErrHandler),
   'up': (_sender, link) => chrome.tabs.create({ url: link || 'chrome://newtab/', active: true }),
-  'down': (sender, link) => {
+  'down': (_sender, link) => {
     chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
       chrome.tabs.create({ url: link || 'chrome://newtab/', active: false, index: tabs[0].index + 1 });
     });
@@ -11,11 +20,8 @@ const gestureActions = {
 };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'performGesture') {
-    const action = gestureActions[request.gesture];
+  if (request.action !== 'performGesture') return
 
-    if (action) {
-      action(sender, request.link);
-    }
-  }
+  const action = gestureActions[request.gesture];
+  if (action) action(sender, request.link);
 });
